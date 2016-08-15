db08babdd043fd1a3e7e6268408a41b8fb7be41a
claudio <claudiobellei@gmail.com>
2016-08-15 20:19:56 +0100

    further cleaning

--- compile.sh
+++ compile.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# gfortran -w constants.f90 multION.f90 sources.f90 ibc.f90 -o mION
-
 if ls *.csv 1> /dev/null 2>&1; then
    echo
    echo "simulation output files found - do you want to delete them? (y/n)"
@@ -23,15 +21,15 @@ echo
 echo "***** starting simulation *****"
 echo
 
-#gfortran -fcheck=all -g -fbacktrace -ffpe-trap=invalid,zero,overflow,underflow -Wall -Wtabs constants.f90 multION.f90 io.f90 utils.f90 ibc.f90 -o multION
-#gfortran -Wall -Wtabs -Wextra -O3 constants.f90 multION.f90 io.f90 utils.f90 ibc.f90 -o multION
+#gfortran -fcheck=all -g -fbacktrace -ffpe-trap=invalid,zero,overflow,underflow -Wall -Wtabs constants.f90 mION.f90 io.f90 utils.f90 ibc.f90 -o mION
+#gfortran -Wall -Wtabs -Wextra -O3 constants.f90 mION.f90 io.f90 utils.f90 ibc.f90 -o mION
 
 
-gfortran -fdefault-real-8 -O3 constants.f90 multION.f90 io.f90 utils.f90 ibc.f90 -o multION -llapack -lblas
-# gfortran -Wall -Wtabs -Wextra -O3 constants.f90 multION.f90 io.f90 utils.f90 ibc.f90 -o multION -llapack -lblas
+gfortran -fdefault-real-8 -O3 constants.f90 mION.f90 io.f90 utils.f90 ibc.f90 -o mION -llapack -lblas
+# gfortran -Wall -Wtabs -Wextra -O3 constants.f90 mION.f90 io.f90 utils.f90 ibc.f90 -o mION -llapack -lblas
 
 if [ $? -ne 0 ]; then
   echo "Errors compiling code"
   exit
 fi
-./multION
+./mION
